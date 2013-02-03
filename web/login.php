91e2293560ef875ba78374c836c3db2cdd6ca947
Masahiro Ide <imasahiro9@gmail.com>
2013-02-04 06:06:42 +0900

    update login.php

--- web/login.php
+++ web/login.php
@@ -2,9 +2,9 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>Twitterでログイン</title>
+<title>Sign in with Twitter</title>
 </head>
 <body>
-<p><a href="redirect.php">Twitterでログイン</a></p>
+<p><a href="redirect.php"><img src="img/sign-in-with-twitter.png" alt="Sign in with Twitter"/></a></p>
 </body>
 </html>
